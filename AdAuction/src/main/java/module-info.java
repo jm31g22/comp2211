6643f93734a8a37febd0e43103257a5925a3ea21
C14IR3 Z. <mz13n22@soton.ac.uk>
2025-03-01 07:35:06 +0000

    Revert "User database setup"
    This reverts commit 43a91cfe22497a275f3802bdfc1610fafaca5433.

--- AdAuction/src/main/java/module-info.java
+++ AdAuction/src/main/java/module-info.java
@@ -10,8 +10,6 @@ module uk.ac.soton.adauction {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
-    requires java.sql;
-
 
     opens uk.ac.soton.adauction.example to javafx.fxml;
     exports uk.ac.soton.adauction.example;
--- AdAuction/src/main/resources/sql/User_Scheme.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `user`  (
-  `userId` int NOT NULL AUTO_INCREMENT,
-  `userName` varchar(20) NOT NULL,
-  `userPwd` varchar(30) NOT NULL,
-  `permission` varchar(10) NOT NULL,
-  PRIMARY KEY (`userId`) USING BTREE
-);
-
