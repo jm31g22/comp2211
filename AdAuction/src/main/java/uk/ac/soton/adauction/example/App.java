d277f36b5354f5cb915802bd24c6b4a8af151ed1
Anderson Kaspe <ak20g23@soton.ac.uk>
2025-04-04 16:53:06 +0100

    Update App.java

--- AdAuction/src/main/java/uk/ac/soton/adauction/example/App.java
+++ AdAuction/src/main/java/uk/ac/soton/adauction/example/App.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.List;
 
 public class App extends Application {
 
@@ -20,16 +21,23 @@ public class App extends Application {
 
     public void start(Stage stage) throws SQLException, IOException {
 
-        // COMMENT THIS LINE OUT IF YOU ALREADY HAVE THE DATABASE ON YOUR MACHINE
-        File dbFile = new File("logs.db");
-        if (!dbFile.exists()){
-            DatabaseCacher.cacheDatabase(DatabaseConnection.getLocalLogsConnection());
+        List<String> args = getParameters().getRaw();
 
-        }
+        long start = System.currentTimeMillis();
         sceneManager = new SceneManager(stage);
+        String pathPrefix = "AdAuction/src/main/java/uk/ac/soton/adauction/example/";
+
+        //Comment this out if you already have db (for quick testing)
+        CampaignImporter.importCampaign(pathPrefix + args.get(0),
+                pathPrefix + args.get(1), pathPrefix + args.get(2));
+
+
         sceneManager.loadScenes();
         sceneManager.switchTo("dashboard");
         stage.show();
+
+        long end = System.currentTimeMillis();
+        System.out.println("Elapsed time: " + (end - start)/1000 + " s");
     }
 
 
